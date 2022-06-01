28323ad287c6f79aa7f7c454b8b5908f06f713d4
jordan <jharrison1@qa.com>
2022-06-01 12:11:29 +0100

    fixed cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const kittenRouter = require('./routes/kitten');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
